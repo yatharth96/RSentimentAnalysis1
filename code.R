70658db937a041260a9f50ab0d80198d4c7a321c
Yatharth Malik <yatharthmalik.96@gmail.com>
2016-12-17 13:16:11 +0530

    Update code.R

--- code.R
+++ code.R
@@ -5,12 +5,13 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
-consumer_key = "TiSNgNuLqwJ6zizM6uuC1ggOg"
-access_token = "443542132-SM0FVcmbabPyTFmf8Fi0BfCCfV5Nbyblm39hT2sB"
-consumer_token = "qrwe5VNUevtrU0t9lvlv0z6w9J97PNosHQ9O8Orn9yFf0EdsYZ"
-acess_secret = "3fEcym0sP3UtnhkhIVvgEGf5zbC646S9B1dDSaFL9doi2"
+# Twitter credentials here
+consumer_key = ###############
+access_token = ###############
+consumer_token = ##############################
+access_secret = ###############
 
-setup_twitter_oauth(consumer_key,consumer_token,access_token,acess_secret)
+setup_twitter_oauth(consumer_key,consumer_token,access_token,access_secret)
 
 
 trump = searchTwitter("Donald Trump+@realDonaldTrump",n = 400 ,lang = "en")
@@ -32,14 +33,14 @@ trump_unique$text = gsub("http\\S+","",trump_unique$text)
 trump_unique$text = gsub("RT","",trump_unique$text)
 trump_unique$text = gsub("[[:punct:]]","",trump_unique$text)
 trump_unique$text = gsub("\\d+\\w+","",trump_unique$text)
-#trump_unique$text = gsub("[.,/-_'-]","",trump_unique$text)
+
 
 clinton_unique$text = gsub("(@|$|#)\\w+","",clinton_unique$text)
 clinton_unique$text = gsub("http\\S+","",clinton_unique$text)
 clinton_unique$text = gsub("RT","",clinton_unique$text)
 clinton_unique$text = gsub("[[:punct:]]","",clinton_unique$text)
 clinton_unique$text = gsub("\\d+\\w+","",clinton_unique$text)
-#clinton_unique$text = gsub("[.,/-_'-]","",clinton_unique$text)
+
 
 
 ## Sentiment Analysis
@@ -53,7 +54,7 @@ trump_request_json = toJSON(list(documents = trump_request))
 response = POST("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
                 body = trump_request_json,
                 add_headers(.headers = c("Content-Type"="application/json",
-                "Ocp-Apim-Subscription-Key"="5155d7c8316641f9986d24dc562da8df")))
+                "Ocp-Apim-Subscription-Key"=###############)))      # Insert Text Analytics API key here
 content_trump = content(response)
 output_trump = data.frame(matrix(unlist(content_trump),nrow = trump_size,byrow = T),stringsAsFactors = F)
 names(output_trump) = c("Sentiment_Score","ID")
@@ -71,7 +72,7 @@ clinton_request_json = toJSON(list(documents = clinton_request))
 response = POST("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment",
                 body = clinton_request_json,
                 add_headers(.headers = c("Content-Type"="application/json",
-                                         "Ocp-Apim-Subscription-Key"="5155d7c8316641f9986d24dc562da8df")))
+                                         "Ocp-Apim-Subscription-Key"=###############)))     ## Insert Text Analytics API key here
 content_clinton = content(response)
 output_clinton = data.frame(matrix(unlist(content_clinton),nrow = clinton_size,byrow = T),stringsAsFactors = F)
 names(output_clinton) = c("Sentiment_Score","ID")
